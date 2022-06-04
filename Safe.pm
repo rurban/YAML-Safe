a6cbd3ec83c04db54ef83d60b8b624f01f30d66b
Reini Urban <rurban@cpan.org>
2022-06-04 08:39:21 +0200

    Release 0.81

--- Safe.pm
+++ Safe.pm
@@ -1,7 +1,7 @@
 use strict; use warnings;
 
 package YAML::Safe;
-our $VERSION = '0.80';
+our $VERSION = '0.81';
 our $XS_VERSION = $VERSION;
 $VERSION = eval $VERSION;
 
@@ -142,7 +142,7 @@ YAML::Safe - Safe Perl YAML Serialization using XS and libyaml
 
 =head1 Description
 
-This module is a refactoring of L<YAML::XS>, the old Perl XS binding
+This module is a re-factoring of L<YAML::XS>, the old Perl XS binding
 to C<libyaml> which offers Perl somewhat acceptable YAML support to
 date.  YAML::XS never produced code which could be read from YAML, and
 thus was unsuitable to be used as YAML replacement for core and CPAN.
